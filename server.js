187107eedfbbe9c820495bdbb91f1c87782b264e
NittarPP <167613469+NittarPP@users.noreply.github.com>
2024-05-06 16:04:13 +0700

    Update and rename api.js to server.js

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-//https://nittarpp.github.io/api.js
+// server.js
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(bodyParser.json());
-
 
 app.post('/api/base64', (req, res) => {
   const base64Data = req.body.data;
   console.log('Received Base64 data:', base64Data);
-
   res.send('Success');
 });
 
